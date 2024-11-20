6ea276b601f7a69022d5ff57c1a8ce7c7865107f
Daniel Idrovo <doidrovo@utpl.edu.ec>
2024-11-20 12:55:52 -0500

    limite

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo02.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo02.java
@@ -17,14 +17,14 @@ public class Ejemplo02 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner entrada = new Scanner(System.in);
-        entrada.useLocale(Locale.US);
+        entrada.useLocale(Locale.US);//Sirve para usar solo el . en los decimales
         int limite;
         int contador = 1;
         System.out.println("Ingrese el Límite");
         limite = entrada.nextInt(); 
         while (contador <= limite){
             
-            if (contador%2!=0) {
+            if (contador%2!=0) { // El if es un condicional y el % es el mod o el residuo de una division
                 System.out.printf("Contador %d\n", 
                     contador);
             }
