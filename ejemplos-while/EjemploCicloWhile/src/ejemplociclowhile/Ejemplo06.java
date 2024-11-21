2d2263b016366723613cda1b27753f4f361d4fe5
Daniel Idrovo <doidrovo@utpl.edu.ec>
2024-11-21 11:36:04 -0500

    bandera

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo06.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo06.java
@@ -30,7 +30,7 @@ public class Ejemplo06 {
         System.out.println("Ingrese las notas de los "
                 + "estudiantes de su materia");
         // true 
-        // while (bandera) {
+        // while (bandera) { escribir solo "bandera" da lo mismo que poner "bandera == true"
         while(bandera==true){
             System.out.println("Ingrese calificación");
             calificacion = entrada.nextDouble();
